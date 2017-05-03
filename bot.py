64c0583e67d4c88986ef3dbf920440f7390e2590
Igor Nabokov <inabokov@65apps.com>
2017-05-03 14:34:16 +0400

    docstrings

--- bot.py
+++ bot.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
+"""telegram bot for enode-cafe.ru"""
 import requests
 from BeautifulSoup import BeautifulSoup
 
-
 def read_file(filename):
+    """read a file"""
     with open(filename) as input_file:
         text = input_file.read()
     return text
 
 def parse_user_datafile_bs(filename):
+    """parse user file"""
     # results = []
     text = read_file(filename)
 
@@ -40,6 +42,7 @@ def parse_user_datafile_bs(filename):
     return result
 
 def get_html():
+    """get html from web"""
     url = 'http://enode-cafe.ru'
     request = requests.get(url)
     with open('test.html', 'w') as output_file:
@@ -53,24 +56,27 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
+    """send welcome message"""
     print("hello")
     bot.reply_to(message, "Ты можешь узнать меню по команде /menu")
 
 @bot.message_handler(commands=['menu'])
 def send_menu(message):
+    """send menu"""
     # bot.reply_to(message, "Howdy, how are you doing?")
     print("sending menu")
     get_html()
     menu = parse_user_datafile_bs('test.html')
     print(menu)
     bot.reply_to(message, menu['title'])
-    for complex in menu['complexes']:
-        bot.reply_to(message, complex)
+    for complex_item in menu['complexes']:
+        bot.reply_to(message, complex_item)
         # for item in complex['content']:
         #     bot.reply_to(message, item)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
+    """echo to all"""
     print("print echo")
     bot.reply_to(message, message.text)
 
